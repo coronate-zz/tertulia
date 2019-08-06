17821571d2429ddfd944823615e1f698d0463859
Alejandro Coronado <acoronadn@gmail.com>
2019-08-06 09:18:13 -0500

    Delete script.js section for contact info, include firestore functionality and account creation in envelope icon

--- landingpage/public/js/firebaselogin.js
+++ landingpage/public/js/firebaselogin.js
@@ -17,13 +17,41 @@ function emailLogin() {
 	console.log(password);
 
 
+	//SAVE INFO IN DATA BASE
+    const db = firebase.firestore();
+    ref = db.collection("alldata").document();
+    myId = ref.getId();
+    console.log("TESTING DB")
+    console.log(myId)
+
+    db.collection("alldata").doc("test2").set({
+        name: " ",
+        email: email,
+        subject: " ",
+        date_of_post: " ",
+        message: " "
+    })
+
+    .then(function() {
+        console.log("Document successfully written!");
+    })
+    .catch(function(error) {
+        console.error("Error writing document: ", error);
+    });
+
+
+	//CREATE NEW USER
 	firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
 	  // Handle Errors here.
 	  console.log(error)
 	  var errorCode = error.code;
 	  var errorMessage = error.message;
 	  // ...
-		})
+		})	
+
+
+
+
 };
 
 
